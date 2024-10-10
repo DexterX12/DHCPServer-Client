e9c1b263feef77ec78e2e89af8d0d428f3bc2349
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2024-10-10 14:21:29 -0500

    DHCP Lease list initial implementation

--- server.c
+++ server.c
@@ -11,13 +11,29 @@
 #define UDP_PACKET_SIZE 576
 #define UDP_SERVER_PORT 67
 
-struct client_data { // child process arguments
+typedef struct { // child process arguments
     int socketfd;
     struct sockaddr* client_addr;
     int client_len;
     char buffer[UDP_PACKET_SIZE];
-};
+} client_data;
 
+typedef struct {
+    uint32_t IP_ADDR; // Client's assigned IP address
+    uint64_t MAC_ADDR; // Client's MAC address
+    int LEASE; // client's assigned lease time
+} addr_lease;
+
+typedef struct {
+    uint32_t NETWORK_ADDR; // Network's identifier
+    uint32_t SUBNET_MASK; // Network's subnet mask
+    uint32_t DNS; // Network's DNS
+    uint32_t DEFAULT_GATEWAY;
+    int LEASE_TIME; // Network's default lease time
+    addr_lease* ADDR_LIST; // Its size will be defined by the subnet mask
+} addr_pool;
+
+addr_pool address_pool;
 
 /******************************* UTILITY FUNCTIONS ********************************/
 
@@ -27,62 +43,95 @@ void error(const char *msg)
     exit(1);
 }
 
-/*
+unsigned char* int_to_ip(uint32_t ip) {
+    unsigned char* ip_bytes = malloc((sizeof(unsigned char) * 4));
+    
+    ip_bytes[0] = ip >> 24;
+    ip_bytes[1] = ip >> 16;
+    ip_bytes[2] = ip >> 8;
+    ip_bytes[3] = ip;
 
-Calculates the amount of bits a subnet mask is using by doing bitwise AND operations
-of every octet of the mask
-
-*/
-
-int calc_subnet_bits(unsigned char* subnet_mask) {
-    int total_bits = 0;
-    unsigned char* subnet;
-    unsigned char* token;
-
-    strcpy(subnet, subnet_mask);
-    token = strtok(subnet, "."); // Splits the subnet mask into the 4 octets
-
-    // Loop while there are more tokens to get 
-    while (token != NULL) {
-        // Each 'token' is the octet which is separated with the delimiter (.)
-        int current_oct = atoi(token);
-
-        /*
-            Comparisons are made by doing a bitwise AND operation with the number 1.
-            if the LSB from the current octet is 1, it means is using that bit, so,
-            the bitwise AND returns 1. Next, the octet is shifted 1 bit to the right
-            to make the comparison with the next LSB.
-
-            The process repeats until all bits are shifted and the value of the octet
-            is 0.
-        */
-        while (current_oct != 0) {
-            total_bits += current_oct & 1;
-            current_oct = current_oct >> 1;
-        }
-
-        // Get the next octet
-        token = strtok(NULL, ".");
-    }
-
-    return total_bits;
+    return ip_bytes;
 }
 
-void get_available_address() {
+uint32_t ip_to_int (unsigned char* ip) {
+    uint32_t ip_int = 0;
+
+    unsigned char* str_base_ip = (char*)malloc(sizeof(ip));
+    char* token;
+    int i = 4;
+
+    strcpy(str_base_ip, ip);
+    token = strtok(str_base_ip, ".");
+
+    while (token != 0) {
+        ip_int += (uint32_t) atoi(token) << 8*(i-1);
+        token = strtok(NULL, ".");
+        i -= 1;
+    }
+
+    return ip_int;
+}
+
+void set_network_params(addr_pool* pool) {
     FILE* file = fopen("../addresses.txt", "r");
+
     unsigned char line[256];
-    unsigned char subnet[256];
     unsigned char* token;
-    int current_line = 1;
 
+    uint32_t network_mask;
+    uint32_t network_address;
+    uint32_t network_gateway;
+    uint32_t network_dns;
+    int network_lease;
+    int available_addresses;
+
+    // Getting the network ip
     fgets(line, sizeof(line), file);
-    strcpy(subnet, line);
-    fclose(file);
+    token = strtok(line, "=");
+    token = strtok(NULL, "=");
+    unsigned char* token2 = strtok(token, "/");
+    unsigned char* ip_str = (char*)malloc(sizeof(token2));
+    strcpy(ip_str, token2);
 
-    token = strtok(subnet, "=");
+    token2 = strtok(NULL, "/");
+    unsigned char* CIDR = (char*)malloc(sizeof(token2));
+    strcpy(CIDR, token2);
+
+    available_addresses = (1 << (32 - atoi(CIDR))) - 2;
+    network_mask = -1 << 32 - atoi(CIDR);
+    network_address = ip_to_int(ip_str);
+    network_address = network_mask & network_address;
+
+    // Getting network's default gateway
+    fgets(line, sizeof(line), file);
+    token = strtok(line, "=");
     token = strtok(NULL, "=");
 
-    printf("%i", calc_subnet_bits(token));
+    network_gateway = ip_to_int(token);
+
+    // Getting network's DNS
+    fgets(line, sizeof(line), file);
+    token = strtok(line, "=");
+    token = strtok(NULL, "=");
+
+    network_dns = ip_to_int(token);
+
+    // Getting network's default lease time
+    fgets(line, sizeof(line), file);
+    token = strtok(line, "=");
+    token = strtok(NULL, "=");
+
+    network_lease = atoi(token);
+
+    fclose(file);
+
+    pool->NETWORK_ADDR = network_address;
+    pool->SUBNET_MASK = network_mask;
+    pool->DEFAULT_GATEWAY = network_gateway;
+    pool->DNS = network_dns;
+    pool->LEASE_TIME = network_lease;
+    pool->ADDR_LIST = malloc(sizeof(addr_lease) * available_addresses);
 }
 
 void serialize_int(unsigned char* buffer, uint64_t value, int offset, int bytes) {
@@ -97,17 +146,6 @@ void serialize_char(unsigned char* buffer,int offset, char value) {
     buffer[offset] = value;
 }
 
-/* The number is created by doing a sum of every byte inside the buffer
-    Every byte represents a value from 0 to 255 (that's why it's an
-    unsigned char), and they are converted to an unsigned int of 64 bit
-    (8 bytes). Since each byte it's just 8 bits, they are always put in the
-    right-most side of the number in binary. Since we are dealing with Big Endian,
-    the most significant bit it's always the first element in pos A and not A+size.
-    So, we shift the current byte to its corresponding position, taking in count
-    that it always starts at the first 8 bits or first byte of the 32 bit number
-    So, if the number is 4 bytes long, buffer[i] << 8*(4-1) shifts its value to
-    the most significant position in a 4 byte number (24 + 8), and so on.
-*/
 uint64_t get_nbyte_number(char* buffer, int offset, int bytes) {
     uint64_t number = 0; // accumulator for final number
     int current_offset = offset; // byte position in buffer where number starts
@@ -124,21 +162,6 @@ uint32_t get_ip(struct sockaddr* ip_address) {
     return (*(struct sockaddr_in*)ip_address).sin_addr.s_addr;
 }
 
-void show_ip_repr(struct sockaddr* ip_address) {
-    uint32_t ip_int = (*(struct sockaddr_in*)ip_address).sin_addr.s_addr;
-    printf("%u\n", ip_int);
-
-    // IPv4 takes 1 byte for every dot (.) separated number from 0 to 255; x.x.x.x 4 bytes
-    unsigned char ip_bytes[4];
-    
-    ip_bytes[0] = ip_int;
-    ip_bytes[1] = ip_int >> 8;
-    ip_bytes[2] = ip_int >> 16;
-    ip_bytes[3] = ip_int >> 24;
-
-    printf("%d.%d.%d.%d\n", ip_bytes[0],ip_bytes[1],ip_bytes[2],ip_bytes[3]);
-}
-
 void print_packet(char* buffer, int size) {
     for (int i = 0; i < size; i++) {
         printf("%02x ", (u_int8_t)buffer[i]);
@@ -146,15 +169,28 @@ void print_packet(char* buffer, int size) {
     printf("\n");
 }
 
-
+void show_network_params(addr_pool* pool) {
+    unsigned char* network_ip_str = int_to_ip(pool->NETWORK_ADDR);
+    unsigned char* network_dns_str = int_to_ip(pool->DNS);
+    unsigned char* network_gateway_str = int_to_ip(pool->DEFAULT_GATEWAY);
+    unsigned char* network_subnet_str = int_to_ip(pool->SUBNET_MASK);
+    
+    printf("%s", "Current parameters for this DHCP server:\n");
+    printf("%s", "Network address: ");
+    printf("%d.%d.%d.%d\n", network_ip_str[0], network_ip_str[1], network_ip_str[2], network_ip_str[3]);
+    printf("%s", "Network subnet mask: ");
+    printf("%d.%d.%d.%d\n", network_subnet_str[0], network_subnet_str[1], network_subnet_str[2], network_subnet_str[3]);
+    printf("%s", "Network default gateway: ");
+    printf("%d.%d.%d.%d\n", network_gateway_str[0], network_gateway_str[1], network_gateway_str[2], network_gateway_str[3]);
+    printf("%s", "Network DNS: ");
+    printf("%d.%d.%d.%d\n", network_dns_str[0], network_dns_str[1], network_dns_str[2], network_dns_str[3]);
+}
 
 /******************************* DHCP OPERATIONS ********************************/
 
-
-
 void* DHCPOffer(void* args) {
-    struct client_data client_args = *(struct client_data*)args;
-    uint32_t offered_ip_dummy = 3232235819;
+    client_data client_args = *(client_data*)args;
+    uint32_t offered_ip_dummy = 3232235806;
 
     serialize_int(client_args.buffer, (u_int64_t) 2, 0, 1);
     serialize_int(client_args.buffer, (u_int64_t) offered_ip_dummy, 20, 4);
@@ -167,8 +203,9 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     socklen_t clientLength;
 
-    get_available_address();
-    exit(0);
+    // set_network_params(&address_pool);
+    // show_network_params(&address_pool);
+    // exit(0);
 
     int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (socketfd < 0)
@@ -191,7 +228,7 @@ int main() {
 
         recvfrom(socketfd, buffer, UDP_PACKET_SIZE, 0, (struct sockaddr*) &client_addr, &clientLength);
 
-        struct client_data th_arg; // Defines an struct for arguments to the child function
+        client_data th_arg; // Defines an struct for arguments to the child function
         th_arg.client_len = clientLength;
         th_arg.client_addr = (struct sockaddr*) &client_addr;
         th_arg.socketfd = socketfd;
